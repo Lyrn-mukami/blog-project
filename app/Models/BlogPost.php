52747c7dacfe478c559e9a34fad4737f6639cac0
Lyrn Mukami <mbotelyrn@gmail.com>
2023-03-16 09:02:12 +0300

    Update: create table relationships

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BlogPost extends Model
 {
     use HasFactory;
+
+    public function User(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function Category():BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/migrations/2023_03_16_054849_create_blog_posts_table.php
+++ database/migrations/2023_03_16_054849_create_blog_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('users_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('categories_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
